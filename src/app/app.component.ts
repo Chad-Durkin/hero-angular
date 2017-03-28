e2756c9229890346742245ce7928230185fc875e
Chad Durkin and Melvin Gruschow <chad+melvin@>
2017-03-28 09:36:42 -0700

    add the ngmodel addon to make 2 way dependancy work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,9 @@ export class Hero {
             <h1>{{title}}</h1>
             <h2>{{hero.name}} details!</h2>
             <div><label>id: </label>{{hero.id}}</div>
-            <div><label>name: </label>
-            <input [(ngModel)]="hero.name" placeholder="name">
+            <div>
+              <label>name: </label>
+              <input [(ngModel)]="hero.name" placeholder="name">
             </div>
             `,
 })
@@ -22,5 +23,18 @@ export class AppComponent  {
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
-  }
+  };
+
+  const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+  ];
 }
